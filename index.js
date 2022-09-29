c17c334790f73320dc7f410230828e32fc373e0e
pinguille0027 <guillermo.orio27@gmail.com>
2022-09-29 10:04:16 +0200

    probando botones

--- index.js
+++ index.js
@@ -43,16 +43,17 @@ form.addEventListener('submit', (e) => {
 
 let themeButton = document.getElementById('theme-toggle')
 let logoImg = document.getElementById('logo')
+let rectanguloecb = document.getElementById("rectangulo encabezado")
 let theme = localStorage.getItem('theme');
 
 if (theme=== 'light') {
     document.documentElement.setAttribute('data-theme', theme);
-    themeButton.innerHTML = '<img width="25" height="25" src="./assets/botón gc.svg" alt="lightTheme-moon-icon">'
+    themeButton.innerHTML = '<img width="50" height="50" src="./assets/botón gc.svg" alt="lightTheme-moon-icon">'
     localStorage.setItem('theme', 'light');
     logo.src='./assets/Logo-light.webp';
 } else {
     document.documentElement.setAttribute('data-theme', 'dark');
-    themeButton.innerHTML = '<img width="25" height="25" src="./assets/botón rc.svg" alt="lightTheme-sun-icon">'
+    themeButton.innerHTML = '<img width="50" height="50" src="./assets/botón rc.svg" alt="lightTheme-sun-icon">'
     theme = 'dark';
     localStorage.setItem('theme', 'dark');
 }
@@ -62,12 +63,14 @@ const switchTheme = () => {
         document.documentElement.setAttribute('data-theme', 'light');
         themeButton.innerHTML = '<img width="50" height="50" src="./assets/botón gr.svg" alt="boton-tema-claro">'
         logo.src='./assets/botón gr.svg';
+        rectanguloecb.src="./assets/Rectangulo gr.svg";
         theme = 'light';
         localStorage.setItem('theme', 'light');
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         themeButton.innerHTML = '<img width="50" height="50" src="./assets/botón rc.svg" alt="boton-tema-oscuro">'
         logo.src='./assets/botón rc.svg';
+        rectanguloecb.src="./assets/Rectangulo rc.svg";
         theme = 'dark';
         localStorage.setItem('theme', 'dark');
     } 

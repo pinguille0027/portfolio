e110d473d7ddc73989e4e189400a0b3b4fca3e58
pinguille0027 <guillermo.orio27@gmail.com>
2022-11-08 17:13:38 +0100

    api7

--- api/form.js
+++ api/form.js
@@ -1,8 +1,8 @@
 //import axios from "axios";
 export default async function handler(request, response) {
     try {
-        console.log(process.env.TELETOKEN)
-        console.log(process.env.TELECHATID)
+        console.log(process.env.TELETOKEN);
+        console.log(process.env.TELECHATID);
         const { email, content } = request.body;
 
         if (!email || !content) { throw err }
@@ -10,9 +10,11 @@ export default async function handler(request, response) {
         const msg = `${email} <br/> ${content}`;
 
         const url = `https://api.telegram.org/bot${process.env.TELETOKEN}/sendMessage?chat_id=${process.env.TELECHATID}&text=${email}`;
-        console.log(url)
+        console.log(url);
         //await axios.get(url)
-        await fetch("https://api.telegram.org/bot5696004560:AAFyGqxj1gVrtKI1kCPSwuqGSLPtBonHz_g/sendMessage?chat_id=1264963875&text=uyu")
+        fetch("https://api.telegram.org/bot5696004560:AAFyGqxj1gVrtKI1kCPSwuqGSLPtBonHz_g/sendMessage?chat_id=1264963875&text=uyu")
+            .then(( res ) => console.log(res))
+            .catch(( err ) => console.log(err));
         response.status(200).send();
 
     } catch(e) {

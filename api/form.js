abbc0a17100eec475588d032606c7293ea389cfb
pinguille0027 <guillermo.orio27@gmail.com>
2022-11-08 17:19:35 +0100

    api9

--- api/form.js
+++ api/form.js
@@ -12,11 +12,14 @@ export default async function handler(request, response) {
         const url = `https://api.telegram.org/bot${process.env.TELETOKEN}/sendMessage?chat_id=${process.env.TELECHATID}&text=${email}`;
         console.log(url);
         //await axios.get(url)
-        await fetch("https://api.telegram.org/bot5696004560:AAFyGqxj1gVrtKI1kCPSwuqGSLPtBonHz_g/sendMessage?chat_id=1264963875&text=uyu", { headers: request.headers });
+        await fetch("https://api.telegram.org/bot5696004560:AAFyGqxj1gVrtKI1kCPSwuqGSLPtBonHz_g/sendMessage?chat_id=1264963875&text=uyu", { 
+            method: 'GET'
+        });
         console.log(url);
         response.status(200).send();
 
     } catch(e) {
+        console.log(e);
         response.status(400).send(e);
     }
 }

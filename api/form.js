dd7aa9ee24d27b00add5146b6cea2050b5c24075
pinguille0027 <guillermo.orio27@gmail.com>
2022-11-08 16:06:29 +0100

    mensajes

--- api/form.js
+++ api/form.js
@@ -1,4 +1,18 @@
+import axios from "axios";
 export default async function handler(request, response) {
-    const { email, content } = request.body;
-    response.status(200).send({email,content})
+    try {
+        const { email, content } = request.body;
+
+        if (!email || !content) { throw err }
+
+        const msg = `${email} <br/> ${content}`;
+
+        const url = `http://api.telegram.org/bot${process.env.TELETOKEN}/sendMessage?chat_id=${process.env.TELECHATID}&parse_mode=markdown&text=${email}`;
+        await axios.get(url)
+
+        response.status(200).send();
+
+    } catch {
+        response.status(400).send();
+    }
 }

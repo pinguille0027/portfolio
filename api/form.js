5f58c48d9d882bb3dc052fd1de38f684b39cee76
pinguille0027 <guillermo.orio27@gmail.com>
2022-11-08 16:39:10 +0100

    api2

--- api/form.js
+++ api/form.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+//import axios from "axios";
 export default async function handler(request, response) {
     try {
         console.log(process.env.TELETOKEN)
@@ -10,8 +10,8 @@ export default async function handler(request, response) {
         const msg = `${email} <br/> ${content}`;
 
         const url = `http://api.telegram.org/bot${process.env.TELETOKEN}/sendMessage?chat_id=${process.env.TELECHATID}&text=${email}`;
-        await axios.get(url)
-
+        //await axios.get(url)
+        await fetch(url)
         response.status(200).send();
 
     } catch {
